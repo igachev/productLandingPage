80bb699b3f9f33a1a11e2adf59493b31f00302f4
igachev <ivophoenix1135@gmail.com>
2022-07-04 16:35:40 +0300

    added more images and media queries

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const navbar = document.querySelector('.nav');
 
 
 let newImg;
-let images = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg']
+let images = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'
+,'img/5.jpg','img/6.jpg','img/7.jpg']
 let index = 0;
 
 //open clicked image
